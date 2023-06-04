fd8a757ef473716ba520a1591842d917a63ec948
https://github.com/Din1998 <dinislam8803@gmail.com>
2023-06-04 18:07:51 +0600

    add mui

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,7 +1,16 @@
-import React from "react";
+'use client'
+
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function NavBar() {
   return(
-    <div>Navbar</div>
+    <div>
+       <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
+    </div>
   )
 }
